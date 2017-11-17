c9c9922163fa68308c0591bea718f5d74c83b22f
tongjixianing <mike.zhang@datastax.com>
2017-11-17 15:52:11 +1100

    Nibbler-0.0.2

---
+++ src/main/java/com/datastax/support/xianingTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c)
+ *
+ * Date: 17/11/2017.
+ *
+ * Author: Chun Gao
+ */
+
+package com.datastax.support;
+
+/**
+ * Created by tongjixianing on 17/11/2017.
+ */
+public class xianingTest {
+}
