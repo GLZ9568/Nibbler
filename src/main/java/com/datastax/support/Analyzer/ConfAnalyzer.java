cd07b0f10d35d973e6c65670228fa5fab3638488
GLZ9568 <cgao9568@gmail.com>
2017-11-25 09:32:32 +1100

    Nibbler-V0.0.1 (ConfAnalyzer)

---
+++ src/main/java/com/datastax/support/Analyzer/ConfAnalyzer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c)
+ *
+ * Date: 24/11/2017
+ *
+ * Author: Chun Gao & Mike Zhang
+ *
+ */
+
+package com.datastax.support.Analyzer;
+
+import com.datastax.support.ConfParser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ * Created by Chun Gao on 17/11/17
+ *
+ * Read corrisponding files and then
+ * 1. extract the information from the files
+ * 2. analyze the extracted information
+ * 3. output the extracted information and recommendations
+ */
+
+public class ConfAnalyzer {
+
+    private static final Logger logger = LogManager.getLogger(ConfAnalyzer.class);
+}
