6834284bd9aa1fd3cb900909c21ed412b6b625fb
tongjixianing <mike.zhang@datastax.com>
2017-12-02 16:04:30 +1100

    GUI v0.0.7 - cluster general info

--- src/main/java/com/datastax/support/Util/StrFactory.java
+++ src/main/java/com/datastax/support/Util/StrFactory.java
@@ -23,11 +23,23 @@ public class StrFactory {
     public static final String file_path = "file_path";
     public static final String opscenterd = "opscenterd";
 
+    public static String iscluster_infoexist = "iscluster_infoexist";
+    public static String isnode_infoexist = "isnode_infoexist";
+    public static String iscpuexist = "iscpuexist";
+    public static String isjava_system_propertiesexist = "isjava_system_propertiesexist";
+    public static String ismachine_infoexist = "ismachine_infoexist";
+
     // file names
     public static final String cassandra_yaml = "cassandra.yaml";
     public static final String address_yaml = "address.yaml";
     public static final String dse_yaml = "dse.yaml";
     public static final String conf_surffix = ".conf";
+    public static final String cluster_info = "cluster_info.json";
+    public static final String node_info = "node_info.json";
+    public static final String cpu= "cpu.json";
+    public static final String java_system_properties = "public static final String.json";
+    public static final String machine_info = "machine_info.json";
+    public static final String ntptime = "ntptime";
 
     // cassandra.yaml configurations
     public static final String seeds = "seeds";
--- src/main/java/com/datastax/test/Test.java
+++ src/main/java/com/datastax/test/Test.java
@@ -30,8 +30,8 @@ public class Test extends Nibbler {
     //private final String testDir = "38949";
     //private final String testDir = "test";
     private final File winDir = new File("D:\\Dropbox (HTG Projects)\\DSE\\02 Tickets\\2017_AP\\" + testDir);
-    private final File linDir = new File ("/Users/cgao/Dropbox (HTG Projects)/DSE/02 Tickets/2017_AP/" + testDir);
-
+   // private final File linDir = new File ("/Users/cgao/Dropbox (HTG Projects)/DSE/02 Tickets/2017_AP/" + testDir);
+    private final File linDir = new File ("/Users/tongjixianing/Downloads/prod_tax-diagnostics-2017_11_24_00_23_20_UTC");
     protected FileFactory ff;
     protected ArrayList<File> files;
 
