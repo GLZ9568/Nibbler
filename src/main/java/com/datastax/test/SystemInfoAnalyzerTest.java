58819d99a1bc62c3416b1e42cdf4fe071d56f908
GLZ9568 <cgao9568@gmail.com>
2018-01-03 11:32:46 +1100

    Nibbler-V0.0.1 (NodetoolInfo Parse, NodetoolStatus Analyzer Added, Parser/Analyzer Modified)

---
+++ src/main/java/com/datastax/test/SystemInfoAnalyzerTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c)
+ *
+ * Date: 26/11/2017
+ *
+ * Author: Chun Gao & Mike Zhang
+ *
+ */
+
+package com.datastax.test;
+
+/**
+ * Created by Chun Gao on 26/11/2017
+ */
+
+public class SystemInfoAnalyzerTest extends Test {
+
+    public static void main (String[] args) {
+
+    }
+}
